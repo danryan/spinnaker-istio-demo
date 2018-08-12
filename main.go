62785be66538916cba20e9cc49b47f04df1fb5bb
Dan Ryan <scriptfu@gmail.com>
2018-08-12 17:39:51 -0400

    bump

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		user := os.Getenv("SAMPLE_APP_USER")
+		c.JSON(200, gin.H{"user": user})
+	})
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	router.Run(":8080")
+}
