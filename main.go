2e403e6b5296b51ca5b585ef1110597121d82ee4
Dan Ryan <scriptfu@gmail.com>
2018-08-16 18:51:59 -0400

    blast

--- main.go
+++ main.go
@@ -13,9 +13,9 @@ func main() {
 		user := os.Getenv("SAMPLE_APP_USER")
 		c.JSON(200, gin.H{"user": user})
 	})
-
+	0
 	router.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+		c.String(200, "KHAAAAAAHN")
 	})
 
 	router.Run(":8080")
